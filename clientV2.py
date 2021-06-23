95209092be4a7b31a8b7cf30c9f6a6131f0aff7a
mxmkiv <57045650+mxmkiv@users.noreply.github.com>
2021-06-23 12:16:43 +0300

    V2 update(nickname func)

--- clientV2.py
+++ clientV2.py
@@ -1,18 +1,37 @@
 import socket
+from threading import Thread
+import pickle
+
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect(('192.168.1.6', 8888))
+username = input('name>')
 
-while True:
-    server_data = client.recv(2048)
-    print('[server]:', server_data.decode('utf-8'))
 
-    message = input('>')
+def listen_server():
+    while True:
+        data = client.recv(2048)
 
-    if message == 'exit':
-        client.send("client disconnect".encode('utf-8'))
-        break
+        if pickle.loads(data)[0] == username:
+            continue
+        else:
+            print(pickle.loads(data)[0], ':', pickle.loads(data)[1])
+        #   (data.decode('utf-8'))
 
-    else:
-        client.send(message.encode('utf-8'))
+
+def send_server():
+    listen_thread = Thread(target=listen_server)
+    listen_thread.start()
+
+    while True:
+        message = input('')
+
+        send_data = [username, message]
+        obj = pickle.dumps(send_data)
+        client.send(obj)
+        #   client.send(input('>').encode('utf-8'))
+
+
+if __name__ == '__main__':
+    send_server()
--- serverV2.py
+++ serverV2.py
@@ -1,4 +1,7 @@
+import pickle
 import socket
+import threading
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -7,23 +10,40 @@ server.bind(('192.168.1.6', 8888))
 server.listen(1)    # разрешение на 1 соединение
 print('listening...')
 
-while True:
-    client_socket, addr = server.accept()
-    print(f'[client info]: {client_socket}')
+users = []
 
-    client_socket.send("connect".encode('utf-8'))
 
-    data = client_socket.recv(2048)
+def send_all(data):
+    for user in users:
+        user.send(data)
 
-    print('[client]:', data.decode('utf-8'))
 
-    message = input('>')
+def listen_user(user):
+    print('listening user')
 
-    if message == 'stop':
-        client_socket.send('socket close'.encode('utf-8'))
-        break
+    while True:
+        data = user.recv(2048)
+        #   print(f'[client]: {data}')
 
-    else:
-        client_socket.send(message.encode('utf-8'))
+        print(pickle.loads(data)[0], ':', pickle.loads(data)[1])
 
-server.close()
+        #   print('[client]:', data.decode('utf-8'))
+        send_all(data)
+
+
+def start_server():
+    while True:
+        user_socket, addr = server.accept()
+        print(f'[info]: {addr[0]} connect')
+
+        users.append(user_socket)
+        listen_accept_user = threading.Thread(
+            target=listen_user,
+            args=(user_socket,)
+        )
+
+        listen_accept_user.start()
+
+
+if __name__ == '__main__':
+    start_server()
